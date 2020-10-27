1589d294c0182180c9f6d15a96ed3a7bb1f5f61b
Scott McAndrew <scottymcandrew@outlook.com>
2020-10-27 10:57:20 +0000

    Removing commented code after successful test.

--- gcp-function/main.py
+++ gcp-function/main.py
@@ -76,49 +76,6 @@ def prisma_trello_card(request):
             },
             "body": request_json
         }
-    # else:   # If received body is a single JSON object
-    #     try:
-    #         account_name = request_json["accountName"]
-    #         severity = request_json["severity"]
-    #         rule_name = request_json["alertRuleName"]
-    #         resource_id = request_json["resourceId"]
-    #         policy_desc = request_json["policyDescription"]
-    #         cloud_resource_type = request_json["resourceCloudService"]
-    #         cloud_type = request_json["cloudType"]
-    #         prisma_alert_url = request_json["callbackUrl"]
-    #
-    #         # Gather key pieces of info from Prisma Alert JSON
-    #         # name given to the card (title)
-    #         name = 'Prisma Alert: ' + "| " + date_time + " | " + resource_id + " " + rule_name
-    #
-    #         # String for the card body
-    #         desc = 'Alert details \n' + "Link to alert: " + prisma_alert_url + "\n" \
-    #                + "Account name: " + account_name + "\n" \
-    #                + "Cloud platform: " + cloud_type + "\n" + "Resource type: " + cloud_resource_type + "\n" \
-    #                + "Policy description: " + policy_desc + "\n"
-    #
-    #         query = {
-    #             'key': secrets_dict[os.environ.get("TRELLO_KEY_SECRET")],
-    #             'token': secrets_dict[os.environ.get("TRELLO_TOKEN_SECRET")],
-    #             'idList': secrets_dict[os.environ.get("TRELLO_IDLIST_SECRET")],
-    #             'pos': 'top',
-    #             'name': name,
-    #             'desc': desc
-    #         }
-    #
-    #         r = http.request('POST', url, fields=query)
-    #         print(r.data)
-    #     except:
-    #         # Reflect the request back as a response. This will greatly aid troubleshooting...
-    #         # so one can analyse the JSON structure against what is expected.
-    #         # This needs to be a 200 response so it will pass the Prisma integration test
-    #         return {
-    #             "statusCode": 200,
-    #             "headers": {
-    #                 "Content-Type": "application/json"
-    #             },
-    #             "body": request_json
-    #         }
 
     return {
         'statusCode': 200
