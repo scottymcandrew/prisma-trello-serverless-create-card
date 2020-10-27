732e30b1ae05096d79b4d0511e96ccbb37c42d4c
Scott McAndrew <scottymcandrew@outlook.com>
2020-10-27 10:28:37 +0000

    Following DRY principle, removing repeated code. Idea is to catch when the JSON request is an object (as opposed to a list of objects) and place into a list. That way the subsequent for loop will always be valid. In this scenario it would simply be one entry at index 0.

--- gcp-function/main.py
+++ gcp-function/main.py
@@ -29,61 +29,19 @@ def prisma_trello_card(request):
     url = "https://api.trello.com/1/cards"
     request_json = request.get_json("body")
 
-    if isinstance(request_json, list):   # If received body is an array of JSON objects
-        try:
-            for counter, value in enumerate(request_json):
-                account_name = request_json[counter]["accountName"]
-                severity = request_json[counter]["severity"]
-                rule_name = request_json[counter]["alertRuleName"]
-                resource_id = request_json[counter]["resourceId"]
-                policy_desc = request_json[counter]["policyDescription"]
-                cloud_resource_type = request_json[counter]["resourceCloudService"]
-                cloud_type = request_json[counter]["cloudType"]
-                prisma_alert_url = request_json[counter]["callbackUrl"]
+    if not isinstance(request_json, list):  # Is this a single JSON object (dict)
+        request_json = [request_json]  # Place into a list
 
-                # Gather key pieces of info from Prisma Alert JSON
-                # name given to the card (title)
-                name = 'Prisma Alert: ' + "| " + date_time + " | " + resource_id + " " + rule_name
-
-                # String for the card body
-                desc = 'Alert details \n' + "Link to alert: " + prisma_alert_url + "\n" \
-                       + "Account name: " + account_name + "\n" \
-                       + "Cloud platform: " + cloud_type + "\n" + "Resource type: " + cloud_resource_type + "\n" \
-                       + "Policy description: " + policy_desc + "\n"
-
-                query = {
-                    'key': secrets_dict[os.environ.get("TRELLO_KEY_SECRET")],
-                    'token': secrets_dict[os.environ.get("TRELLO_TOKEN_SECRET")],
-                    'idList': secrets_dict[os.environ.get("TRELLO_IDLIST_SECRET")],
-                    'pos': 'top',
-                    'name': name,
-                    'desc': desc
-                }
-
-                r = http.request('POST', url, fields=query)
-                print(r.data)
-
-        except:
-            # Reflect the request back as a response. This will greatly aid troubleshooting...
-            # so one can analyse the JSON structure against what is expected.
-            # This needs to be a 200 response so it will pass the Prisma integration test
-            return {
-                "statusCode": 200,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "body": request_json
-            }
-    else:   # If received body is a single JSON object
-        try:
-            account_name = request_json["accountName"]
-            severity = request_json["severity"]
-            rule_name = request_json["alertRuleName"]
-            resource_id = request_json["resourceId"]
-            policy_desc = request_json["policyDescription"]
-            cloud_resource_type = request_json["resourceCloudService"]
-            cloud_type = request_json["cloudType"]
-            prisma_alert_url = request_json["callbackUrl"]
+    try:
+        for counter, value in enumerate(request_json):
+            account_name = request_json[counter]["accountName"]
+            severity = request_json[counter]["severity"]
+            rule_name = request_json[counter]["alertRuleName"]
+            resource_id = request_json[counter]["resourceId"]
+            policy_desc = request_json[counter]["policyDescription"]
+            cloud_resource_type = request_json[counter]["resourceCloudService"]
+            cloud_type = request_json[counter]["cloudType"]
+            prisma_alert_url = request_json[counter]["callbackUrl"]
 
             # Gather key pieces of info from Prisma Alert JSON
             # name given to the card (title)
@@ -106,17 +64,61 @@ def prisma_trello_card(request):
 
             r = http.request('POST', url, fields=query)
             print(r.data)
-        except:
-            # Reflect the request back as a response. This will greatly aid troubleshooting...
-            # so one can analyse the JSON structure against what is expected.
-            # This needs to be a 200 response so it will pass the Prisma integration test
-            return {
-                "statusCode": 200,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "body": request_json
-            }
+
+    except:
+        # Reflect the request back as a response. This will greatly aid troubleshooting...
+        # so one can analyse the JSON structure against what is expected.
+        # This needs to be a 200 response so it will pass the Prisma integration test
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": request_json
+        }
+    # else:   # If received body is a single JSON object
+    #     try:
+    #         account_name = request_json["accountName"]
+    #         severity = request_json["severity"]
+    #         rule_name = request_json["alertRuleName"]
+    #         resource_id = request_json["resourceId"]
+    #         policy_desc = request_json["policyDescription"]
+    #         cloud_resource_type = request_json["resourceCloudService"]
+    #         cloud_type = request_json["cloudType"]
+    #         prisma_alert_url = request_json["callbackUrl"]
+    #
+    #         # Gather key pieces of info from Prisma Alert JSON
+    #         # name given to the card (title)
+    #         name = 'Prisma Alert: ' + "| " + date_time + " | " + resource_id + " " + rule_name
+    #
+    #         # String for the card body
+    #         desc = 'Alert details \n' + "Link to alert: " + prisma_alert_url + "\n" \
+    #                + "Account name: " + account_name + "\n" \
+    #                + "Cloud platform: " + cloud_type + "\n" + "Resource type: " + cloud_resource_type + "\n" \
+    #                + "Policy description: " + policy_desc + "\n"
+    #
+    #         query = {
+    #             'key': secrets_dict[os.environ.get("TRELLO_KEY_SECRET")],
+    #             'token': secrets_dict[os.environ.get("TRELLO_TOKEN_SECRET")],
+    #             'idList': secrets_dict[os.environ.get("TRELLO_IDLIST_SECRET")],
+    #             'pos': 'top',
+    #             'name': name,
+    #             'desc': desc
+    #         }
+    #
+    #         r = http.request('POST', url, fields=query)
+    #         print(r.data)
+    #     except:
+    #         # Reflect the request back as a response. This will greatly aid troubleshooting...
+    #         # so one can analyse the JSON structure against what is expected.
+    #         # This needs to be a 200 response so it will pass the Prisma integration test
+    #         return {
+    #             "statusCode": 200,
+    #             "headers": {
+    #                 "Content-Type": "application/json"
+    #             },
+    #             "body": request_json
+    #         }
 
     return {
         'statusCode': 200
