448cdb6c0cc04da2f4369990f2945dd45b96f35e
Scott McAndrew <scottymcandrew@outlook.com>
2020-11-04 10:59:23 +0000

    Updated Lambda to use the new For Loop functionality

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -9,18 +9,49 @@ def lambda_handler(event, context):
     url = "https://api.trello.com/1/cards"
     data = json.loads(event['body'])
 
+    now = datetime.now()
+    date_time = now.strftime("%Y/%m/%d, %H:%M:%S")
+
+    if not isinstance(data, list):  # Is this a single JSON object (dict)
+        data = [data]  # Place into a list
+
     # Gather key pieces of info from Prisma Alert JSON
     try:
-        account_name = data["accountName"]
-        severity = data["severity"]
-        rule_name = data["alertRuleName"]
-        resource_id = data["resourceId"]
-        policy_desc = data["policyDescription"]
-        cloud_resource_type = data["resourceCloudService"]
-        cloud_type = data["cloudType"]
-        prisma_alert_url = data["callbackUrl"]
-        now = datetime.now()
-        date_time = now.strftime("%Y/%m/%d, %H:%M:%S")
+        for counter, value in enumerate(data):
+            account_name = data[counter]["accountName"]
+            severity = data[counter]["severity"]
+            rule_name = data[counter]["alertRuleName"]
+            resource_id = data[counter]["resourceId"]
+            policy_desc = data[counter]["policyDescription"]
+            cloud_resource_type = data[counter]["resourceCloudService"]
+            cloud_type = data[counter]["cloudType"]
+            prisma_alert_url = data[counter]["callbackUrl"]
+
+            # name given to the card (title)
+            name = 'Prisma Alert: ' + "| " + date_time + " | " + resource_id + " " + rule_name
+
+            # String for the card body
+            desc = 'Alert details \n' + "Link to alert: " + prisma_alert_url + "\n" \
+                   + "Account name: " + account_name + "\n" \
+                   + "Cloud platform: " + cloud_type + "\n" + "Resource type: " + cloud_resource_type + "\n" \
+                   + "Policy description: " + policy_desc + "\n"
+
+            query = {
+                'key': os.environ.get("key"),
+                'token': os.environ.get("token"),
+                'idList': os.environ.get("idList"),
+                'pos': 'top',
+                'name': name,
+                'desc': desc
+            }
+
+            r = http.request(
+                'POST',
+                url,
+                fields=query)
+
+            print(r.data)
+
     except:
         # Reflect the request back as a response. This will greatly aid troubleshooting...
         # so one can analyse the JSON structure against what is expected.
@@ -31,31 +62,6 @@ def lambda_handler(event, context):
             },
             "body": data
         }
-
-    # name given to the card (title)
-    name = 'Prisma Alert: ' + "| " + date_time + " | " + resource_id + " " + rule_name
-
-    # String for the card body
-    desc = 'Alert details \n' + "Link to alert: " + prisma_alert_url + "\n" \
-           + "Account name: " + account_name + "\n" \
-           + "Cloud platform: " + cloud_type + "\n" + "Resource type: " + cloud_resource_type + "\n" \
-           + "Policy description: " + policy_desc + "\n"
-
-    query = {
-        'key': os.environ.get("key"),
-        'token': os.environ.get("token"),
-        'idList': os.environ.get("idList"),
-        'pos': 'top',
-        'name': name,
-        'desc': desc
-    }
-
-    r = http.request(
-        'POST',
-        url,
-        fields=query)
-
-    print(r.data)
 
     return {
         'statusCode': 200,
